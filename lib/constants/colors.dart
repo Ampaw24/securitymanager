9bdd2b9c10a751274d9ca3890b628d8549d57286
Ampaw24 <ampawsafo22@gmail.com>
2023-09-03 14:23:53 +0000

    inial commit

---
+++ lib/constants/colors.dart
@@ -0,0 +1,17 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+
+Map<String, Color> GreenishgDarkTheme = {
+  'themeColorDark':Color(0xff13262E),
+      'cardWhite' : Color(0xffD9D9D9),
+      'ballWhite' : Color(0xffC6C5C5),
+      'gtextDark' : Color(0xff050E0D),
+      'formfieldwhite':Color(0xffEEE6E6),
+      'button':Color(0xff142831),
+
+};
+
+
+
+
